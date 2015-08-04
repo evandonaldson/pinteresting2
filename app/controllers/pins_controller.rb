1e25db8e279024f6bb9c6574fc34c0ab0b7dae02
Evan Donaldson <evan.donaldson@hotmail.com>
2015-08-04 15:18:40 +1000

    Pins and Users can't sign in as user so can't asign pins or users to pins

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,5 +1,6 @@
 class PinsController < ApplicationController
-  before_action :set_pin, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
+end
 
   
   def index
@@ -11,7 +12,7 @@ class PinsController < ApplicationController
   end
 
   def new
-    @pin = Pin.new
+    @pin = current_user.pins.build
   end
 
 
@@ -20,8 +21,7 @@ class PinsController < ApplicationController
 
 
   def create
-    @pin = Pin.new(pin_params)
-
+    @pin = current_user.pins.build(pin_params)
       if @pin.save
         redirect_to @pin, notice: 'Pin was successfully created.' 
       else
